d99c0a25dfb41e87b52ec793ae60b7a737bf87a3
xibsx <xibstech@gmail.com>
2026-02-01 14:12:02 +0300

    Change MEGA email and password in mega.js
    Updated MEGA authentication credentials.

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: 'your mega email',       // your MEGA email
-    password: 'password',    // your MEGA password
+    email: 'justicegeaz3@gmail.com',       // your MEGA email
+    password: 'justicegeaz3@gmail.comjusticegeaz3@gmail.com',    // your MEGA password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
